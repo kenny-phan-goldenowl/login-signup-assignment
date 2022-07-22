b96a195e05da202302140d657cb3aa4ee10392cf
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-07-22 16:28:02 +0700

    add simple validation rules

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const loginForm = document.getElementById('loginForm')
-const signUpForm = document.getElementById('signUpForm')
-const passError = document.getElementById('password-error')
-const mailError = document.getElementById('email-error')
-const fname = document.getElementById('fname')
-const smail = document.getElementById('sign-up-mail')
-const spassword = document.getElementById('sign-up-password')
-const scpassword = document.getElementById('sign-up-cpassword')
-const cpassError = document.getElementById('cpass-error')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let loginForm = document.getElementById('loginForm')
+let signUpForm = document.getElementById('signUpForm')
+let passError = document.getElementById('password-error')
+let mailError = document.getElementById('email-error')
+let fname = document.getElementById('fname')
+let smail = document.getElementById('sign-up-mail')
+let spassword = document.getElementById('sign-up-password')
+let scpassword = document.getElementById('sign-up-cpassword')
 
 loginForm.addEventListener("submit", e => {
+    const userMail = email.value;
+    const userPass = password.value;
+    let output = JSON.parse(localStorage.getItem(userMail))
     e.preventDefault();
     if (email.value === "" || email.value == null) {
         mailError.innerText = "Email is required"
-    }
+    } else {mailError.innerText = ""}
 
     if (password.value.length === 0) {
         passError.innerText = "Password must not be empty"
-    }
+    } else {passError.innerText = ""}
 
-    if (email.value === "abc" && password.value === "123") {
-        window.location.href = 'landingpage.html';
+    if (userMail === output.email && userPass === output.password) {
+        window.location.href = '/html/landingpage.html';
     } else {
         alert("Wrong email or password")
     }
-
 })
-
 
 
 
